0a86e2d02310e8dcaf3afd0403ea7ac0b612bf22
Tássio <86972667+26Tassiofernandes@users.noreply.github.com>
2022-12-11 17:42:49 -0300

    Update formulario.js

--- formulario.js
+++ formulario.js
@@ -1,22 +1,19 @@
-campos = ['nome', 'sobrenome', 'email', 'exp', 'mensagem']
+campos = ['nome', 'sobrenome', 'email', 'experiencia', 'msg']
 for (item in campos) {
     document.getElementById(campos[item]) /*Seleciona cada elemento por ID */
 }
 
 function send() {
     
-    if (nome.value.length == 0 || sobrenome.value.length == 0 || email.value.length == 0 || exp.value.length == 0 
+    if (nome.value.length == 0 || sobrenome.value.length == 0 || email.value.length == 0 || experiencia.value.length == 0 
         || !document.getElementById('front').checked && !document.getElementById('back').checked && !document.getElementById('full').checked 
-        || !document.getElementById('inc').checked && !document.getElementById('int').checked && !document.getElementById('avç').checked 
-        || !document.getElementById('py').checked && !document.getElementById('java').checked && !document.getElementById('js').checked 
-        && !document.getElementById('html').checked && !document.getElementById('css').checked && !document.getElementById('csharp').checked 
-        && !document.getElementById('php').checked) {
+        || !document.getElementById('inc').checked && !document.getElementById('int').checked && !document.getElementById('avç').checked || !document.getElementById('py').checked && !document.getElementById('java').checked && !document.getElementById('js').checked && !document.getElementById('html').checked && !document.getElementById('css').checked && !document.getElementById('csharp').checked && !document.getElementById('php').checked) {
 
         window.alert('Campo(s) vazio(s)! Tente novamente...')
         
     } else {
 
-        mensagem.innerHTML = `Tudo certo, ${nome.value}, o formulário já foi enviado ao RH e em breve entraremos em contato pelo seu e-mail fornecido.`
+        msg.innerHTML = `Tudo certo, ${nome.value}, o formulário já foi enviado ao RH e em breve entraremos em contato pelo seu e-mail fornecido. Boa sorte!`
 
     }
     
@@ -24,11 +21,10 @@ function send() {
 
 function resetar() {
     campos = ['nome', 'sobrenome', 'email', 'experiencia']
-        for (item in campos) {
-            document.getElementById(campos[item]).value = ''
+    for (item in campos) {
+        document.getElementById(campos[item]).value = '' /* Reseta os campos de texto*/
     }
     
-    
-    mensagem.innerHTML = ''
+    msg.innerHTML = ''
     
 }
